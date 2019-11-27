1127bedeb689fa162b6b76c8a33386ff5535cc1a
Michał Skorzec <skorzecmichal@gmail.com>
2019-11-27 20:49:23 +0100

    initial local commit. after cloning

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './main.css';
+
+import { changeToFiveDays } from './modules/weatherFiveDays/fiveDays.js';
+import { changeToPresentDay } from './modules/weatherNow/nowDays.js';
+import { changeToOneDay } from './modules/weatherOneDay/oneDay.js';
+
+const body = document.body;
+const nowDay = document.getElementById('nowDay');
+const oneDay = document.getElementById('oneDay');
+const fiveDays = document.getElementById('fiveDays');
+// body.addEventListener('click', change);
+nowDay.addEventListener('click', changeToPresentDay);
+oneDay.addEventListener('click', changeToFiveDays);
+fiveDays.addEventListener('click', changeToOneDay);
---
+++ src/modules/weatherFiveDays/fiveDays.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+const p = document.querySelector('.main');
+// eslint-disable-next-line no-undef
+// const div = document.querySelector('.div');
+
+// eslint-disable-next-line import/prefer-default-export
+export const changeToFiveDays = function() {
+ p.textContent = 'piec dni';
+};
