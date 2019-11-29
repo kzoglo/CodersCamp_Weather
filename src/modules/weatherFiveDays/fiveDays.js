4c4c9a87bd8456395d543816a7c0089cba0de7f6
Oskar <oskar.dawid4@gmail.com>
2019-11-29 20:22:25 +0100

    5days

--- src/modules/weatherFiveDays/fiveDays.js
+++ src/modules/weatherFiveDays/fiveDays.js
@@ -1,9 +1,111 @@
-// eslint-disable-next-line no-undef
-const p = document.querySelector('.main');
-// eslint-disable-next-line no-undef
-// const div = document.querySelector('.div');
-
-// eslint-disable-next-line import/prefer-default-export
-export const changeToFiveDays = function() {
- p.textContent = 'aesfs';
+const plday= (a)=>{
+    if(a==0){
+        return "Niedziela"
+    }
+    if(a==1){
+        return "Poniedziałek"
+    }
+    if(a==2){
+        return "Wtorek"
+    }
+    if(a==3){
+        return "Środa"
+    }
+    if(a==4){
+        return "Czwartek"
+    }
+    if(a==5){
+        return "Piątek"
+    }
+    if(a==6){
+        return "Sobota"
+    }
+}
+export const changeToOneDay = function() {
+    const x = fetchFromApi('Wrocław');
+    mainContainer.innerHTML = '<object type="text/html" data="/src/modules/weatherOneDay/oneDay.html" ></object>';;
+    // mainContainer.textContent = x;
+    console.log(x)
 };
+const apiKey = '18ab372b1fc6ef784c0191c7e088d9a2';
+const query = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+const units = 'metric'
+
+function fetchFromApi(cityInput){
+fetch(`${query}${cityInput}&APPID=${apiKey}&units=${units}`)
+   .then(resp => resp.json()) 
+   .then(resp => {
+        const a =(resp.list[0].main.temp);//temperatura;
+        const b =(resp.list[7].main.temp)
+        const c =(resp.list[15].main.temp)
+        const d =(resp.list[23].main.temp)
+        const e =(resp.list[31].main.temp)
+        const f =(resp.list[39].main.temp)
+        aa=Math.round(a);
+        bb=Math.round(b);
+        cc=Math.round(c);
+        dd=Math.round(d);
+        ee=Math.round(e);
+        // console.log(aa);
+        // console.log(bb);
+        // console.log(cc);
+        // console.log(dd);
+        // console.log(e);
+        
+        let daya =new Date(resp.list[0].dt_txt).getUTCDay()
+        let dayb =new Date(resp.list[7].dt_txt).getUTCDay()
+        let dayc =new Date(resp.list[15].dt_txt).getUTCDay()
+        let dayd =new Date(resp.list[23].dt_txt).getUTCDay()
+        let daye =new Date(resp.list[31].dt_txt).getUTCDay()
+        let dayf =new Date(resp.list[39].dt_txt).getUTCDay()
+
+        let pldaya= plday(daya)
+        let pldayb= plday(dayb)
+        let pldayd= plday(dayd)
+        let pldayc= plday(dayc)
+        let pldaye= plday(daye)
+        console.log(pldaya)
+        console.log(pldayb)
+        console.log(pldayc)
+        console.log(pldayd)
+        console.log(pldaye)
+       
+
+
+
+    let chart= document.getElementById('myChart').getContext('2d');
+   let day= new Chart (myChart, {   
+    type:'line',
+       
+       data:{
+           labels:[pldaya,pldayb,pldayc,pldayd,pldaye ],
+           datasets:[{
+               label:'Temperatura',
+               backgroundColor:'#f9f9c5',
+               borderColor:'#ffbe1c',
+               data:[
+                   a,
+                   b,
+                   c,
+                   d,
+                   e
+               ]
+           }],
+           
+
+        },
+        options:{
+            scales:{
+                yAxes:[{
+                    ticks:{ 
+                        beginAtZero: true,
+
+                    }
+                }]
+
+            }
+        }
+    })
+    .catch(reject => console.log('Rejected: ' + reject))
+   }
+,)}
