1b04c584058021291b1889e0490a7959ee49533b
Klaudia <gora.klaudia@gmail.com>
2019-11-30 01:04:58 +0100

    create canvas charts, wind small view

--- src/modules/weatherNow/nowDays.js
+++ src/modules/weatherNow/nowDays.js
@@ -6,7 +6,6 @@ const mainContainer = document.querySelector('.main');
 // eslint-disable-next-line import/prefer-default-export
 export const changeToPresentDay = async function() {
  const x = fetchFromApi('Wrocław');
- 
 };
 
 const apiKey = 'd50a614e489fbba6669358f04ee95daa';
@@ -16,38 +15,94 @@ const units = 'metric';
 function fetchFromApi(cityInput) {
  fetch(`${query}${cityInput}&APPID=${apiKey}&units=${units}`)
   .then(result => result.json())
-  .then(result => {render(result); console.log(getTodaysObject(result))})
+  .then(result => render(result))
   .catch(reject => console.log(`Rejected: ${reject}`));
 }
 
 function render(x) {
  $.get('/src/modules/weatherNow/nowDays.mst', function(template) {
-  console.log(x)
+  console.log(x);
   const result = Mustache.to_html(template, x.list[0]);
   $('.main').html(result);
-  let todayDate = getTodaysDate();
- document.getElementById('todayDate').textContent = todayDate;
+  afterRender(x);
  });
 }
 
+function afterRender(result) {
+ document.getElementById('todayDate').textContent = getTodaysDate();
+ const todayObjects = getTodaysObject(result);
+ createTempChar(todayObjects);
+
+ document.getElementById('temp').addEventListener('click', function(){createTempChar(todayObjects)});
+ document.getElementById('rain').addEventListener('click', createRainChar);
+ document.getElementById('wind').addEventListener('click', function(){createWindChar(result);});
+
+}
+
 function getTodaysObject(x) {
-  let todayDate = getTodaysDate()
-//  document.getElementById('secBox').addEventListener('click', function () {
-//    console.log('smile');
-//  })
+ let todayDate = getTodaysDate();
  const todayObjects = x.list.filter(date => {
   if (date.dt_txt.includes(todayDate)) {
-    console.log(date);
-
-    return date;
+   return date;
   }
  });
+ console.log(todayObjects)
  return todayObjects;
 }
 
 function getTodaysDate() {
-  const d = new Date();
-  const todayDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
-  console.log(document.getElementById('todayDate'));
-  return todayDate;
+ const d = new Date();
+ const todayDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+ return todayDate;
 }
+
+function createTempChar(todayObjects) {
+  $("#chartContainer").html("");
+  console.log(todayObjects)
+  const h = todayObjects.map(x => [x.dt_txt.split(' ')[1].split(':')[0], x.main.temp])
+  let tableObjectOfh = []
+  for(let i=0; i<h.length; i++) {
+    let objectOfh = {x:h[i][0], y:h[i][1]}
+    tableObjectOfh.push(objectOfh);
+  }
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2",
+    axisY:{
+      includeZero: false,
+      title: 'Temperatura'
+    },
+    axisX: {
+      interval: 2,
+      minimum: -1,
+      maximum: 24, 
+      title: 'Godzina',
+    },
+    data: [{        
+      type: "line",       
+      dataPoints: tableObjectOfh,
+    }]
+  });
+
+  chart.render();
+}
+
+function createWindChar(x) {
+  $("#chartContainer").html("");
+  $.get('/src/modules/weatherNow/wind.mst', function(template) {
+    console.log(x)
+    const result = Mustache.to_html(template, x.list[0]);
+    $('#chartContainer').html(result);
+    $(".fa-location-arrow")[0].style.cssText = `--wind-deg: ${x.list[0].wind.deg}deg`;
+   });
+  console.log('wind');
+}
+
+function createRainChar() {
+  $("#chartContainer").html("");
+  console.log('rain');
+}
+
+ //  document.getElementById('secBox').addEventListener('click', function () {
+ //    console.log('smile');
+ //  })
