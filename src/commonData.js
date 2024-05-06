76c2d1fd050c6e69aae40a3702422d74ad31fcea
Kamil Żogło <zoglokamil@gmail.com>
2024-05-06 20:08:52 +0200

    dist files for deploy

--- src/commonData.js
+++
@@ -1,4 +0,0 @@
-export const apiKey = '8b3bc422d7ecc88dd713aec0c91b6c1d'
-export const url = 'https://api.openweathermap.org/data/2.5/forecast?id='
-export const units = 'metric'
-export const wroclawId = 3081368
--- src/vendor.js
+++
@@ -1,3 +0,0 @@
-// PLACE FOR IMPORTS OF LIBRARIES
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
