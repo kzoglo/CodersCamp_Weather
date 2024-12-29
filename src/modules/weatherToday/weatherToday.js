252c36cef0a27c38d2f3dd50fe8e73f91e8b9dc1
Kamil Żogło <zoglokamil@gmail.com>
2024-12-29 16:06:05 +0100

    weather today - month fix

--- src/modules/weatherToday/weatherToday.js
+++ src/modules/weatherToday/weatherToday.js
@@ -18,7 +18,7 @@ function getTodayDate() {
   let todayDate = ''
 
   const year = `${date.getFullYear()}`
-  const month = date.getDay() < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`
+  const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`
   const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`
 
   todayDate = `${year}-${month}-${day}`
